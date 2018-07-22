12b2c9a713811637c4d80e438ccda4e506a0fac8
Thomas Shaddox <wediddoit@gmail.com>
2018-07-22 03:08:29 -0700

    refactor the caches

--- src/color/ColorSpace/ColorSpace.js
+++ src/color/ColorSpace/ColorSpace.js
@@ -7,9 +7,7 @@ import mapInterval from "../../utils/mapInterval";
 import memoize from "lodash/memoize";
 import size from "lodash/size";
 
-const USE_CACHE = true;
-const COLOR_CACHE = {};
-const SCALE_CACHE = {};
+const USE_CACHE = false;
 
 const makeCache = (getKey, makeObj) => {
   const store = {};
@@ -33,39 +31,50 @@ const makeCache = (getKey, makeObj) => {
   };
   return cacheObj;
 };
+
+const COLOR_CACHE = makeCache(
+  (space, args) => [space.key, ...Object.values(args)].join(","),
+  (space, args) => space._make(args)
+);
+
+const SCALE_CACHE = makeCache(
+  (axis, color, resolution) => {
+    const args = { ...color.args };
+    delete args[axis.key];
+    return [axis.space.key, axis.key, resolution, ...Object.values(args)].join(
+      ","
+    );
+  },
+  (axis, color, resolution) => axis._scale(color, resolution)
+);
+
 export const makeColorSpace = (key, space) => {
   const {
-    axes,
-    toPositionalArgs,
-    fromPositionalArgs,
     validator = noop,
     chromaConstructor = chroma[key],
     chromaConverter = chromaColor => chromaColor[key](),
   } = space;
 
-  const getCacheKey = positionalArgs => [key, ...positionalArgs].join(",");
   const spaceObj = {
     ...space,
+    validator,
+    chromaConstructor,
+    chromaConverter,
     make(args) {
-      const positionalArgs = toPositionalArgs(this.roundArgs(args));
+      return COLOR_CACHE.get(this, args);
+    },
+    _make(args) {
+      const positionalArgs = this.toPositionalArgs(this.roundArgs(args));
+      const chromaColor = this.chromaConstructor(...positionalArgs);
+      const that = this;
 
-      let cacheKey;
-      if (USE_CACHE) {
-        cacheKey = getCacheKey(positionalArgs);
-        if (COLOR_CACHE[cacheKey]) {
-          // console.log("cache hit", cacheKey);
-          return COLOR_CACHE[cacheKey];
-        }
-      }
-
-      const chromaColor = chromaConstructor(...positionalArgs);
       const color = {
-        space: this,
+        space: that,
         chromaColor,
         args,
         positionalArgs,
         isValid: memoize(function() {
-          const override = validator(chromaColor);
+          const override = that.validator(chromaColor);
           if (override === true || override === false) {
             // console.log("override", override, chromaColor);
             // return override;
@@ -78,17 +87,15 @@ export const makeColorSpace = (key, space) => {
           return true;
         }),
       };
-      if (USE_CACHE) {
-        COLOR_CACHE[cacheKey] = color;
-      }
+
       return color;
     },
     args(color) {
       if (color.space === this) {
         return color.args;
       } else {
-        const positionalArgs = chromaConverter(color.chromaColor);
-        const args = fromPositionalArgs(positionalArgs);
+        const positionalArgs = this.chromaConverter(color.chromaColor);
+        const args = this.fromPositionalArgs(positionalArgs);
         return this.roundArgs(args);
       }
     },
@@ -123,7 +130,7 @@ export const makeColorSpace = (key, space) => {
     },
   };
 
-  spaceObj.axes = mapValues(axes, function(axis, axisKey) {
+  spaceObj.axes = mapValues(spaceObj.axes, function(axis, axisKey) {
     const [min, max] = axis.range;
     const size = max - min;
     const orderOfMagnitude = Math.floor(Math.log10(size));
@@ -148,91 +155,14 @@ export const makeColorSpace = (key, space) => {
 
     const round = x => lodashRound(x, precision);
 
-    const getCacheKey = (color, resolution) => {
-      const args = { ...color.args };
-      delete args[axis.key];
-      return [key, axis.key, resolution, ...Object.values(args)].join(",");
-    };
-
-    const scale = function(color, resolution) {
-      let cacheKey;
-      if (USE_CACHE) {
-        cacheKey = getCacheKey(color, resolution);
-        if (SCALE_CACHE[cacheKey]) {
-          return SCALE_CACHE[cacheKey];
-        }
-      }
-
-      const colors = times(resolution, function(i) {
-        const color_ = spaceObj.replace(color, {
-          [axis.key]: mapInterval(i, 0, resolution - 1, min, max),
-        });
-        return color_;
-      });
-      if (USE_CACHE) {
-        SCALE_CACHE[cacheKey] = colors;
-      }
-      return colors;
-    };
-
-    const graphWith = function(
-      yAxis,
-      rowCount = Math.round(yAxis.steps),
-      colCount = Math.round(steps)
-    ) {
-      return function(color) {
-        // const rowCount = Math.round(yAxis.steps * downsample);
-        // const colCount = Math.round(steps * downsample);
-        const rows = yAxis.scale(color, rowCount);
-
-        // `graph` is a 2-d array (an array of rows, where each row is an
-        // array of colors).
-        const graph = rows.map(rowColor => scale(rowColor, colCount));
-
-        graph.neighbors = ([rowIndex, colIndex]) => {
-          const offsets = [
-            // Keep this formatting.
-            [-1, 0], // up
-            [0, 1], // right
-            [1, 0], // down
-            [0, -1], // left
-            // Diagonals:
-            [-1, -1], // NW
-            [-1, 1], // NE
-            [1, 1], // SE
-            [1, -1], // SW
-          ];
-          const neighbs = [];
-          offsets.forEach(([i, j]) => {
-            const r = rowIndex + i;
-            const c = colIndex + j;
-            if (r >= 0 && c >= 0 && r < rowCount && c < colCount) {
-              neighbs.push(graph[r][c]);
-            }
-          });
-          return neighbs;
-        };
-        graph.neighbors = memoize(graph.neighbors);
-
-        graph.isOnValidityBorder = ([rowIndex, colIndex]) => {
-          const color = graph[rowIndex][colIndex];
-          return graph
-            .neighbors([rowIndex, colIndex])
-            .some(neighborColor => color.isValid() !== neighborColor.isValid());
-        };
-        graph.isOnValidityBorder = memoize(graph.isOnValidityBorder);
-
-        return graph;
-      };
-    };
-
-    const graphWithMemoized = memoize(graphWith, (...args) => args);
+    // const graphWithMemoized = memoize(graphWith, (...args) => args);
 
     return {
       ...axis,
       // TODO: this seems to not work, and the annotate loop in Color.js is
       // still necessary for some reason.
       key: axisKey,
+      space: spaceObj,
       min,
       max,
       size,
@@ -241,8 +171,73 @@ export const makeColorSpace = (key, space) => {
       step,
       steps,
       round,
-      scale,
-      graphWith: graphWithMemoized,
+      _scale(color, resolution) {
+        const that = this;
+        const colors = times(resolution, function(i) {
+          const color_ = that.space.replace(color, {
+            [axis.key]: mapInterval(i, 0, resolution - 1, that.min, that.max),
+          });
+          return color_;
+        });
+        return colors;
+      },
+      scale(color, resolution) {
+        return SCALE_CACHE.get(this, color, resolution);
+      },
+      graphWith(
+        yAxis,
+        rowCount = Math.round(yAxis.steps),
+        colCount = Math.round(steps)
+      ) {
+        const that = this;
+        return function(color) {
+          // const rowCount = Math.round(yAxis.steps * downsample);
+          // const colCount = Math.round(steps * downsample);
+          const rows = yAxis.scale(color, rowCount);
+
+          // `graph` is a 2-d array (an array of rows, where each row is an
+          // array of colors).
+          const graph = rows.map(rowColor => that.scale(rowColor, colCount));
+
+          graph.neighbors = ([rowIndex, colIndex]) => {
+            const offsets = [
+              // Keep this formatting.
+              [-1, 0], // up
+              [0, 1], // right
+              [1, 0], // down
+              [0, -1], // left
+              // Diagonals:
+              [-1, -1], // NW
+              [-1, 1], // NE
+              [1, 1], // SE
+              [1, -1], // SW
+            ];
+            const neighbs = [];
+            offsets.forEach(([i, j]) => {
+              const r = rowIndex + i;
+              const c = colIndex + j;
+              if (r >= 0 && c >= 0 && r < rowCount && c < colCount) {
+                neighbs.push(graph[r][c]);
+              }
+            });
+            return neighbs;
+          };
+          graph.neighbors = memoize(graph.neighbors);
+
+          graph.isOnValidityBorder = ([rowIndex, colIndex]) => {
+            const color = graph[rowIndex][colIndex];
+            return graph
+              .neighbors([rowIndex, colIndex])
+              .some(
+                neighborColor => color.isValid() !== neighborColor.isValid()
+              );
+          };
+          graph.isOnValidityBorder = memoize(graph.isOnValidityBorder);
+
+          return graph;
+        };
+      },
+      // graphWith: memoize(this._graphWith, (...args) => args),
     };
   });
 
